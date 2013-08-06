453854ee01db4e9b5a7814319c14c565f0799345
Rock <liangrockman@gmail.com>
2013-08-06 11:54:50 +0800

    add .gitignore

---
+++ src/main/java/com/inhome/sif/SensitiveInfoFilterServlet.java
@@ -0,0 +1,24 @@
+package com.inhome.sif;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liang
+ * Date: 13-8-6
+ * Time: 上午11:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class SensitiveInfoFilterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! Rock9</h2>
+</body>
+</html>
