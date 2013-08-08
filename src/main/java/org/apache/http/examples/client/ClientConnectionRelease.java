b4b6552b702cfbc0a0991ffd8d727e6282ebbe36
Rock <liangrockman@gmail.com>
2013-08-08 15:07:12 +0800

    none

--- src/main/java/org/apache/http/examples/client/ClientConnectionRelease.java
+++ src/main/java/org/apache/http/examples/client/ClientConnectionRelease.java
@@ -46,7 +46,7 @@ public class ClientConnectionRelease {
         HttpClient httpclient = new DefaultHttpClient();
         try {
             HttpGet httpget = new HttpGet("http://hc.apache.org/index.html");
-            // Test Test
+            // Test Test  Referer
             httpget.addHeader("Referer", "http://liangrockman.gicp.net:8080/sc_home.html?abc=bbc");
 
             // Execute HTTP request
