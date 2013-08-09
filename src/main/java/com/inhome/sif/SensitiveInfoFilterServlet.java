3e767dc7a1f0f0824726e905b74d77bbc1e7e4c9
Administrator <526822342@qq.com>
2013-08-09 22:25:40 +0800

    none

--- src/main/java/com/inhome/sif/SensitiveInfoFilterServlet.java
+++ src/main/java/com/inhome/sif/SensitiveInfoFilterServlet.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,36 +25,58 @@ public class SensitiveInfoFilterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
+        //Query String参数
+        Map mapparam = new HashMap();
         for( Enumeration e = request.getParameterNames(); e.hasMoreElements(); ){
              String paramName = (String)e.nextElement();
+             String paramValue = request.getParameter(paramName);
              out.println("<div>" + paramName+ ":" + request.getParameter(paramName) + "</div>");
+             mapparam.put(paramName,paramValue);
         }
 
-        String referer = request.getHeader("Referer");
-        ArrayList cookieList = new ArrayList();
-        HttpServletRequest rep = request;
-
-        Cookie[] e = rep.getCookies();
-        for(Cookie s : e){
-            String cookieValue = (String)s.getValue();
-            cookieList.add(cookieValue);
-            out.print(cookieValue);
-
+        //遍历Query String参数
+        System.out.println("Query String参数：");
+        Set paramKeys = mapparam.keySet();
+        Iterator paramItr = paramKeys.iterator();
+        while ( true ){
+            if ( false == paramItr.hasNext() ){
+                break;
+            }
+            String key = ( String )paramItr.next();
+            String value = ( String )mapparam.get( key );
+            System.out.println( key + " ： " + value );
         }
 
-        /*
-        ArrayList cookieList2 = new ArrayList();
-        for(int i = 0; i < e.length; i++){
-
-
+        //Http报头
+        Map mapHeader = new HashMap();
+        System.out.println("Http报头内容：");
+        for(Enumeration e = request.getHeaderNames(); e.hasMoreElements(); ){
+            String headerName = (String)e.nextElement();
+            String headerValue = request.getHeader(headerName);
+           // System.out.println(headerName + ":" + headerValue);
+            mapHeader.put(headerName,headerValue);
         }
-               */
 
-       /* String[] a = {"1","2"};
-        for(String s : a){
+        //遍历Http报头
+        Set headerKeys = mapHeader.keySet();
+        Iterator headerItr = headerKeys.iterator();
+        while ( true ){
+            if ( false == headerItr.hasNext() ){
+                break;
+            }
+            String key = ( String )headerItr.next();
+            String value = ( String )mapHeader.get( key );
+            System.out.println( key + " ： " + value );
+        }
 
-        }           */
 
+
+        FilterList fl = new FilterList();
+        for(Iterator<String> it = fl.list.iterator(); it.hasNext(); ){
+            String s = it.next();
+           /* String newRef = referer.replace(s, "123");
+            System.out.println(newRef);*/
+        }
 
         try {
             ClientConnectionRelease.main(new String[]{});
