fb6184ed64e2cd7a759bd6cb9c6a3dc0df91c084
Administrator <526822342@qq.com>
2013-08-09 13:35:56 +0800

    none

---
+++ src/main/java/com/inhome/sif/FilterList.java
@@ -0,0 +1,22 @@
+package com.inhome.sif;
+
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-8-8
+ * Time: 下午8:09
+ * To change this template use File | Settings | File Templates.
+ */
+class FilterList {
+    /*InputStream is = new FileInputStream("");
+    byte[] buffer = new byte[100];
+    int length = 0;
+    while ((length = is.read(buffer, 0, 100)) != -1) {
+        String str = new String(buffer, 0, length);   //将字节数组转换成字符串
+        System.out.print(str);
+    }
+    is.close();
+                  */
+}
