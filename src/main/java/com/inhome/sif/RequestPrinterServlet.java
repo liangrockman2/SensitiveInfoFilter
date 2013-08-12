9d1440d78c2262845475832420573260e7979be1
liangrockman2 <liangrockman_001@163.com>
2013-08-12 09:30:12 +0800

    Merge branch 'master' of https://github.com/liangrockman/SensitiveInfoFilter into liangrockman-master
    Conflicts:
    	src/main/java/org/apache/http/examples/client/ClientConnectionRelease.java

--- src/main/java/com/inhome/sif/RequestPrinterServlet.java
+++ src/main/java/com/inhome/sif/RequestPrinterServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,10 +16,20 @@ import java.io.IOException;
  */
 public class RequestPrinterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("---------------Request Printer------------------");
+        System.out.println("URI:"+request.getRequestURI());
+        System.out.println("QUERY:"+request.getQueryString());
 
+        for(Enumeration headerNames = request.getHeaderNames();  headerNames.hasMoreElements(); ){
+            String header = (String)headerNames.nextElement();
+            String value = request.getHeader(header);
+            System.out.println("(header)"+header+":"+value);
+        }
+
+        System.out.print("------------------------------------------------");
     }
 }
